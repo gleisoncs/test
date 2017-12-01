4513d551bc945268e12e01bc0ef1d531ac094155
gleisoncs <gleisoncs@gmail.com>
2017-12-01 17:58:46 +0000

    initial version of joda test

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Main {
+
+	public static void main(String[] argv) {
+        
+		DateTimeFormatter dtf = DateTimeFormat.forPattern("ddMMMYYYY");
+		DateTimeZone zone = DateTimeZone.forID("Australia/Melbourne");
+
+		DateTime thisDate = dt1.parseDateTime("30Jun2013");
+		// correctly prints "EST"
+		System.out.println("\nZone: " + zone.getShortName(thisDate.getMillis()));
+		
+		
+		System.exit(0);
+		
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String dateInString = "2001-05-01 12:34:56.789";
+
+        try {
+            Date date = formatter.parse(dateInString);
+            Date date1 = convertDateIngivenTimezoneReport(date, null, -120);
+            System.out.println(date1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+	
+	public static Date convertDateIngivenTimezoneReport(Date dateInServerTz, String timezone, int browserTzOffset) {
+		DateTime dateToConvertFromTimeZone = new DateTime(dateInServerTz);
+		DateTime dateConvertedGivenTimeZone = null;
+		TimeZone preferredTimeZone = TimeZone.getDefault();
+		
+		if (timezone != null){
+			preferredTimeZone = TimeZone.getTimeZone(timezone);
+			dateConvertedGivenTimeZone = dateToConvertFromTimeZone.withZone(DateTimeZone.forTimeZone(preferredTimeZone));
+		}else{
+			DateTime dstDateTime = dateToConvertFromTimeZone.withZone(DateTimeZone.forOffsetMillis(browserTzOffset*60*1000));
+			preferredTimeZone = TimeZone.getTimeZone(dstDateTime.getZone().toTimeZone().getID());
+			System.out.println(preferredTimeZone.getDSTSavings());
+			dateConvertedGivenTimeZone = dateToConvertFromTimeZone.withZone(DateTimeZone.forTimeZone(preferredTimeZone));			
+		}
+		return dateConvertedGivenTimeZone.toLocalDateTime().toDate();
+	}
+}
